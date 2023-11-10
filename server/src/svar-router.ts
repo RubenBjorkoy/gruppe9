56989f0899c8b7c013418dd081dcd230cf1e0c03
Ruben Vega Bjørkøy <rubenvb@stud.ntnu.no>
2023-11-10 13:16:28 +0100

    Updated object handling on the routers to match client objects

--- server/src/svar-router.ts
+++ server/src/svar-router.ts
@@ -26,8 +26,8 @@ router.get('/sporsmal/:sporsmalid/svar/:svarid', (request, response) => {
 // Example request body: { title: "Ny oppgave" }
 // Example response body: { id: 4 }
 router.post('/sporsmal/:sporsmalid/svar', (request, response) => {
-  if(request.params.sporsmalid != request.body.svar.sporsmalid) response.status(400).send('Sporsmalid in url and body does not match');
-  const data = request.body.svar;
+  if(request.params.sporsmalid != request.body.sporsmalid) response.status(400).send('Sporsmalid in url and body does not match');
+  const data = request.body;
   data.dato = new Date(data.dato);
   data.sistendret = new Date(data.sistendret);
   if (data && data.svartekst && data.svartekst.length != 0)
@@ -39,7 +39,7 @@ router.post('/sporsmal/:sporsmalid/svar', (request, response) => {
 });
 
 router.put('/sporsmal/:sporsmalid/svar', (request, response) => {
-  const data = request.body.svar
+  const data = request.body
   if (
     typeof data.svarid == 'number' &&
     typeof data.svartekst == 'string' &&
