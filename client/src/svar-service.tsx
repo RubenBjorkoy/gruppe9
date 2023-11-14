0ab467a2b17563c6439508739d130152aebe3038
Seaweedbeard <ruben.bjorkoy@gmail.com>
2023-11-14 22:29:54 +0100

    Fixed creating an answer from questions

--- client/src/svar-service.tsx
+++ client/src/svar-service.tsx
@@ -1,56 +1,72 @@
-import axios from 'axios';
+import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:3000/api/v2';
+axios.defaults.baseURL = "http://localhost:3000/api/v2";
 
 export type Svar = {
-  svarid?: number; //svarid is handled by the database
-  svartekst: string;
-  poeng: number;
-  sporsmalid: number;
-  erbest: boolean;
-  dato: Date;
-  sistendret: Date;
-  ersvar: boolean;
-  svarsvarid?: number | null; //svarsvarid is based on if ersvar is true or false
+	svarid?: number; //svarid is handled by the database
+	svartekst: string;
+	poeng: number;
+	sporsmalid: number;
+	erbest: boolean;
+	dato: Date;
+	sistendret: Date;
+	ersvar: boolean;
+	svarsvarid?: number | null; //svarsvarid is based on if ersvar is true or false
 };
 
 class SvarService {
-  /**
-   * Get answers with given sporsmalid and svarid.
-   */
-  get(sporsmalid: number, svarid: number) {
-    return axios.get<Svar>('/sporsmal/' + svarid).then((response) => response.data);
-  }
+	/**
+	 * Get answers with given sporsmalid and svarid.
+	 */
+	get(sporsmalid: number, svarid: number) {
+		return axios
+			.get<Svar>("/sporsmal/" + svarid)
+			.then((response) => response.data);
+	}
 
-  /**
-   * Get all answers to a given question.
-   */
-  getAll(sporsmalid: number) {
-    return axios.get<Svar[]>(`/sporsmal/${sporsmalid}/svar`).then((response) => response.data);
-  }
+	/**
+	 * Get all answers to a given question.
+	 */
+	getAll(sporsmalid: number) {
+		return axios
+			.get<Svar[]>(`/sporsmal/${sporsmalid}/svar`)
+			.then((response) => response.data);
+	}
 
-  /**
-   * Create new Answer to a given question.
-   *
-   * Resolves the newly created svarid.
-   */
-   create(svartekst: string, sporsmalid: number, poeng: number, ersvar: boolean, svarsvarid?: number) {
-    return axios
-      .post<{ svar: Svar }>('/sporsmal', { svartekst: svartekst, sporsmalid: sporsmalid, poeng: poeng, ersvar: ersvar, svarsvarid: svarsvarid})
-      .then((response) => response.data);
-  }
+	/**
+	 * Create new Answer to a given question.
+	 *
+	 * Resolves the newly created svarid.
+	 */
+	create(
+		svartekst: string,
+		sporsmalid: number,
+		poeng: number,
+		ersvar: boolean,
+		svarsvarid?: number
+	) {
+		return axios
+			.post<{ svar: Svar }>(`/sporsmal/${sporsmalid}/svar`, {
+				svartekst: svartekst,
+				sporsmalid: sporsmalid,
+				poeng: poeng,
+				ersvar: ersvar,
+				svarsvarid: svarsvarid ? svarsvarid : null,
+			})
+			.then((response) => response.data);
+	}
 
-  delete(svarid: number) {
-    return axios
-    .delete<Svar>('/sporsmal/' + svarid)
-    .then((response) => response.data);
-    }
-    
-  put(svarid: number) {
-      return axios
-      .put<Svar>('/sporsmal/' + svarid)
-      .then((response) => response.data);
-      }
+	delete(svarid: number) {
+		return axios
+			.delete<Svar>("/sporsmal/" + svarid)
+			.then((response) => response.data);
+	}
+
+	put(svarid: number) {
+		return axios
+			.put<Svar>("/sporsmal/" + svarid)
+			.then((response) => response.data);
+	}
 }
 
 const svarService = new SvarService();
--- server/src/svar-router.ts
+++ server/src/svar-router.ts
@@ -1,65 +1,71 @@
-import express from 'express';
-import svarService from './svar-service';
+import express from "express";
+import svarService from "./svar-service";
 
 /**
  * Express router containing task methods.
  */
 const router = express.Router();
 
-router.get('/sporsmal/:sporsmalid/svar', (request, response) => {
-  const sporsmalid: number = Number(request.params.sporsmalid);
-  svarService
-    .getAll(sporsmalid)
-    .then((rows) => response.send(rows))
-    .catch((error) => response.status(500).send(error));
+router.get("/sporsmal/:sporsmalid/svar", (request, response) => {
+	const sporsmalid: number = Number(request.params.sporsmalid);
+	svarService
+		.getAll(sporsmalid)
+		.then((rows) => response.send(rows))
+		.catch((error) => response.status(500).send(error));
 });
 
-router.get('/sporsmal/:sporsmalid/svar/:svarid', (request, response) => {
-    const sporsmalid: number = Number(request.params.sporsmalid);
-    const svarid: number = Number(request.params.svarid);
-  svarService
-    .get(sporsmalid, svarid)
-    .then((svar) => {(svar ? response.send(svar) : response.status(404).send('Svar not found'))})
-    .catch((error) => response.status(500).send(error));
+router.get("/sporsmal/:sporsmalid/svar/:svarid", (request, response) => {
+	const sporsmalid: number = Number(request.params.sporsmalid);
+	const svarid: number = Number(request.params.svarid);
+	svarService
+		.get(sporsmalid, svarid)
+		.then((svar) => {
+			svar ? response.send(svar) : response.status(404).send("Svar not found");
+		})
+		.catch((error) => response.status(500).send(error));
 });
 
 // Example request body: { title: "Ny oppgave" }
 // Example response body: { id: 4 }
-router.post('/sporsmal/:sporsmalid/svar', (request, response) => {
-  if(request.params.sporsmalid != request.body.sporsmalid) response.status(400).send('Sporsmalid in url and body does not match');
-  const data = request.body;
-  data.dato = new Date(data.dato);
-  data.sistendret = new Date(data.sistendret);
-  if (data && data.svartekst && data.svartekst.length != 0)
-    svarService
-      .create(data)
-      .then((id) => response.send({ id: id }))
-      .catch((error) => {response.status(500).send(error)});
-  else response.status(400).send('Missing comment title');
+router.post("/sporsmal/:sporsmalid/svar", (request, response) => {
+	if (request.params.sporsmalid != request.body.sporsmalid)
+		response.status(400).send("Sporsmalid in url and body does not match");
+	const data = request.body;
+	const roundedDate = new Date(Math.floor(Date.now() / 1000) * 1000);
+	data.dato = new Date(roundedDate);
+	data.sistendret = new Date(roundedDate);
+	if (data && data.svartekst && data.svartekst.length != 0)
+		svarService
+			.create(data)
+			.then((id) => response.send({ id: id }))
+			.catch((error) => {
+				response.status(500).send(error);
+			});
+	else response.status(400).send("Missing comment title");
 });
 
-router.put('/sporsmal/:sporsmalid/svar', (request, response) => {
-  const data = request.body
-  if (
-    typeof data.svarid == 'number' &&
-    typeof data.svartekst == 'string' &&
-    data.svartekst.length > 0 &&
-    typeof data.poeng == 'number' &&
-    typeof data.sporsmalid == 'number'
-  ) {
-    svarService
-      .update(data)
-      .then(() => response.send())
-      .catch((error) => response.status(500).send(error));
-  } else response.status(400).send('Missing comment id');
+router.put("/sporsmal/:sporsmalid/svar", (request, response) => {
+	const data = request.body;
+	if (
+		typeof data.svarid == "number" &&
+		typeof data.svartekst == "string" &&
+		data.svartekst.length > 0 &&
+		typeof data.poeng == "number" &&
+		typeof data.sporsmalid == "number"
+	) {
+		svarService
+			.update(data)
+			.then(() => response.send())
+			.catch((error) => response.status(500).send(error));
+	} else response.status(400).send("Missing comment id");
 });
 
-router.delete('/sporsmal/:sporsmalid/svar/:svarid', (request, response) => {
-  const svarid: number = Number(request.params.svarid);
-  svarService
-    .delete(svarid)
-    .then((_result) => response.send())
-    .catch((error) => response.status(500).send(error));
+router.delete("/sporsmal/:sporsmalid/svar/:svarid", (request, response) => {
+	const svarid: number = Number(request.params.svarid);
+	svarService
+		.delete(svarid)
+		.then((_result) => response.send())
+		.catch((error) => response.status(500).send(error));
 });
 
 export default router;
