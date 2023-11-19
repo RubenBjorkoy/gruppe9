c57fd067ac332156c2483f22c5614c42e29b24a1
Seaweedbeard <ruben.bjorkoy@gmail.com>
2023-11-19 14:04:02 +0100

    Fixed server tests not working

--- server/src/favoritt-service.ts
+++ server/src/favoritt-service.ts
@@ -3,17 +3,6 @@ import type { RowDataPacket, ResultSetHeader } from "mysql2";
 import { Svar } from "./svar-service";
 
 class FavorittService {
-	query(sql: string, values: any): Promise<RowDataPacket[]> {
-		return new Promise((resolve, reject) => {
-			pool.query(sql, values, (error, results: RowDataPacket[]) => {
-				if (error) {
-					reject(error);
-				} else {
-					resolve(results);
-				}
-			});
-		});
-	}
 	/**
 	 * Get all answers also located in favorites table.
 	 */
@@ -40,39 +29,49 @@ class FavorittService {
 	/**
 	 * Create new question having the given title.
 	 *
+	 * Checks first if the question already exists on Favoritt table.
+	 * If it does, it will reject the promise.
+	 * If it does not, it will check if the Answer to be favorited even exists
+	 * If it does not exist, then it will reject the promise.
+	 *
+	 * If said question does exist, then it will insert the question into the Favoritt table.
+	 *
 	 * Resolves the newly created question id.
 	 */
 	create(svarid: Number) {
-		return new Promise<number>(async (resolve, reject) => {
-			try {
-				// Check if relation already exists
-				const favoritterResults = await this.query(
-					"SELECT * FROM Favoritter WHERE svarid = ?",
-					[svarid]
-				);
-				if (favoritterResults.length > 0) {
-					return reject("Already favorited");
+		return new Promise<number>((resolve, reject) => {
+			pool.query(
+				"SELECT * FROM Favoritter WHERE svarid = ?",
+				[svarid],
+				(error, results: RowDataPacket[]) => {
+					if (error) return reject(error);
+					if (results.length > 0) {
+						return reject("Already favorited");
+					} else {
+						pool.query(
+							"SELECT * FROM Svar WHERE svarid = ?",
+							[svarid],
+							(error, results: RowDataPacket[]) => {
+								if (error) return reject(error);
+								if (results.length === 0) {
+									return reject("Answer does not exist");
+								} else {
+									pool.query(
+										"INSERT INTO Favoritter(svarid) VALUES (?)",
+										[svarid],
+										(error, results: ResultSetHeader) => {
+											if (error) {
+												return reject(error);
+											}
+											resolve(results.insertId);
+										}
+									);
+								}
+							}
+						);
+					}
 				}
-
-				// Check if answer exists
-				const svarResults = await this.query(
-					"SELECT * FROM Svar WHERE svarid = ?",
-					[svarid]
-				);
-				if (svarResults.length === 0) {
-					return reject("Answer does not exist");
-				}
-
-				// Execute the INSERT query
-				const insertResults = await this.query(
-					"INSERT INTO Favoritter(svarid) VALUES (?)",
-					[svarid]
-				);
-
-				resolve(insertResults[0].insertId);
-			} catch (error) {
-				reject(error);
-			}
+			);
 		});
 	}
 
